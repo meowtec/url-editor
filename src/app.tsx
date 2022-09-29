2ae87192bad8b7b45c5ae1dbc377904230d3162b
meowtec <bertonzh@gmail.com>
2022-09-29 16:14:57 +0800

    feat: use JSON5 & store url in page search

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,48 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import JSON from 'json5';
+import debounce from 'debounce';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import CodeEditor from './code';
-import './app.css';
 import { parse, stringify } from './parse';
 import Links from './links';
+import './app.css';
+
+const getUrlFromSearch = () => {
+  const url = new URL(location.href).searchParams.get('url') ?? '';
+  try {
+    // validate url
+    new URL(url);
+    return url;
+  } catch {
+    return '';
+  }
+};
+
+const setUrlToSearch = debounce((url: string) => {
+  const pageUrl = new URL(location.href);
+  pageUrl.searchParams.set('url', url);
+  history.replaceState(
+    { url},
+    '',
+    pageUrl.href
+  );
+}, 1000);
+
+const urlToJSONString = (url: string) => JSON.stringify(parse(url), null, '  ');
 
 export default function App() {
-  const [url, setUrl] = useState('');
-  const [code, setCode] = useState('');
+  const [url, setUrl] = useState(() => getUrlFromSearch());
+  const [code, setCode] = useState(() => urlToJSONString(url));
+
+  const updateCodeFromUrl = (urlStr: string) => {
+    try {
+      setCode(urlToJSONString(urlStr));
+    } catch {}
+  };
 
   const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setUrl(value);
-    try {
-      setCode(JSON.stringify(parse(value), null, '  '));
-    } catch {}
+    updateCodeFromUrl(value);
   };
 
   const handleCodeChange = useCallback((code: string) => {
@@ -23,15 +52,19 @@ export default function App() {
     } catch {}
   }, []);
 
+  useEffect(() => {
+    setUrlToSearch(url);
+  }, [url]);
+
   return (
-    <div className="app">
+    <div className='app'>
       <input
         value={url}
-        className="input"
-        placeholder="输入待解析 URL"
+        className='input'
+        placeholder='Input URL here'
         onChange={handleUrlChange}
       />
-      <CodeEditor className="editor" value={code} onChange={handleCodeChange} />
+      <CodeEditor className='editor' value={code} onChange={handleCodeChange} />
       <Links />
     </div>
   );
